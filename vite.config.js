56fdcc0fd9bc0852409cb6f3b1508526023548a7
guyandjack <guillaume-d@tutanota.com>
2024-06-30 12:26:13 +0200

    deploy on git hub

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/coaching-life/",
   plugins: [react()],
   serveur: {
     open: "./src/HTML/fr/prestations-de-coaching-individuel-et-en-entreprise.html",
